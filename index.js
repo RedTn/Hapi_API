e48ae83988b4898bd6c821f05f23bb963830e741
RedTn <redmond.tran@gmail.com>
2017-07-03 00:34:12 -0700

    simple rest api

--- index.js
+++ index.js
@@ -21,6 +21,46 @@ server.route([
         handler: function(request, reply) {
             reply("Hello world from Hapi.");
         }
+    },
+    {
+        method: 'GET',
+        path: '/api/v1/todoList',
+        handler: function(request, reply) {
+            reply(todoList);
+        }
+    },
+    {
+        method: 'POST',
+        path: '/api/v1/todoList',
+        handler: function(request, reply) {
+            newTask = {"task":request.payload.task, "owner":request.payload.owner};
+            todoList.push(newTask);
+            reply(todoList).code(201);
+        }
+    },
+    {
+        method: 'GET',
+        path: '/api/v1/todoList/{index}',
+        handler: function(request, reply) {
+            reply(todoList[request.params.index-1]);
+        }
+    },
+    {
+        method: 'PUT',
+        path: '/api/v1/todoList/{index}',
+        handler: function(request, reply) {
+            newTask = {"task":request.payload.task, "owner":request.payload.owner};
+            todoList[request.params.index-1] = newTask;
+            reply(todoList[request.params.index-1]);
+        }
+    },
+    {
+        method: 'DELETE',
+        path: '/api/v1/todoList/{index}',
+        handler: function(request, reply) {
+            delete todoList[request.params.index-1];
+            reply().code(204);
+        }
     }
 ]);
 
